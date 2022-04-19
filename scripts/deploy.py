99759068ec463004b4ef7f1c04a2ea55399aa8bc
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-04-19 13:24:25 +0200

    feat: set a max dust count in grid

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,6 +4,7 @@ from nile.core.call_or_invoke import call_or_invoke
 
 SPACE_SIZE = 20
 MAX_TURN = 50
+MAX_DUST = 30
 
 def run(nre : NileRuntimeEnvironment):
 
@@ -16,7 +17,7 @@ def run(nre : NileRuntimeEnvironment):
     rand, _ = nre.deploy("rand", [])
     space, _ = nre.deploy("space")
     dust, _ = nre.deploy("dust", [space, rand])
-    nre.invoke(space, "initialize", [f"{dust}", f"{SPACE_SIZE}", f"{MAX_TURN}"])
+    nre.invoke(space, "initialize", [f"{dust}", f"{SPACE_SIZE}", f"{MAX_TURN}", f"{MAX_DUST}"])
     basic_ship, _ = nre.deploy("basic_ship", [])
 
     print(f"rand contract address: {rand}")
