21c8434ea5e9aea1a8896e5b5360788584d85b93
Stanislas Bernard <bernardstanislas@gmail.com>
2022-04-22 17:58:00 +0200

    refactor(space test): rename state assertion functions

--- tests/test_space.py
+++ tests/test_space.py
@@ -49,7 +49,7 @@ async def test_get_first_non_empty_cell(space_factory):
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 1), Vector2(0, 1))
+    await assert_dust_state(dust, 1, Vector2(0, 1), Vector2(0, 1))
 
     execution_info = await space.get_first_non_empty_cell(0, 0).call()
     (px, py), (lo, hi), ship = execution_info.result.cell
@@ -79,7 +79,7 @@ async def test_get_first_empty_cell(space_factory):
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 1), Vector2(0, 1))
+    await assert_dust_state(dust, 1, Vector2(0, 1), Vector2(0, 1))
 
     execution_info = await space.get_first_empty_cell(0, 0).call()
     (px, py) = execution_info.result.position
@@ -125,50 +125,50 @@ async def test_next_turn_no_ship(space_factory):
     space, dust = space_factory
 
     # Assert grid is empty
-    await assert_grid_dust(space, 0, 0, [], [])
+    await assert_grid_state(space, [], [])
 
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 1), Vector2(0, 1))
-    await assert_grid_dust(space, 0, 0, [Dust(Vector2(0, 1), 1)], [])
+    await assert_dust_state(dust, 1, Vector2(0, 1), Vector2(0, 1))
+    await assert_grid_state(space, [Dust(Vector2(0, 1), 1)], [])
 
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 2), Vector2(0, 1))
-    await assert_dust_position(dust, 2, Vector2(0, 4), Vector2(0, -1))
-    await assert_grid_dust(space, 0, 0, [Dust(Vector2(0, 2), 1), Dust(Vector2(0, 4), 2)], [])
+    await assert_dust_state(dust, 1, Vector2(0, 2), Vector2(0, 1))
+    await assert_dust_state(dust, 2, Vector2(0, 4), Vector2(0, -1))
+    await assert_grid_state(space, [Dust(Vector2(0, 2), 1), Dust(Vector2(0, 4), 2)], [])
 
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 3), Vector2(0, 1))
-    await assert_dust_position(dust, 2, Vector2(0, 3), Vector2(0, -1))
+    await assert_dust_state(dust, 1, Vector2(0, 3), Vector2(0, 1))
+    await assert_dust_state(dust, 2, Vector2(0, 3), Vector2(0, -1))
     # MAX_DUST == 2 so no more dust has been spawned
 
     # A collision occured, assert the dust was burnt
     await assert_revert(dust.ownerOf(to_uint(1)).call(), reverted_with='ERC721: owner query for nonexistent token')
 
-    await assert_grid_dust(space, 0, 0, [Dust(Vector2(0, 3), 1)], [])
+    await assert_grid_state(space, [Dust(Vector2(0, 3), 1)], [])
 
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 4), Vector2(0, 1))
+    await assert_dust_state(dust, 1, Vector2(0, 4), Vector2(0, 1))
     # a new dust was spawned
-    await assert_dust_position(dust, 3, Vector2(5, 1), Vector2(0, 1))
+    await assert_dust_state(dust, 3, Vector2(5, 1), Vector2(0, 1))
 
-    await assert_grid_dust(space, 0, 0, [Dust(Vector2(0, 4), 1), Dust(Vector2(5, 1), 3)], [])
+    await assert_grid_state(space, [Dust(Vector2(0, 4), 1), Dust(Vector2(5, 1), 3)], [])
 
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 5), Vector2(0, 1))
-    await assert_dust_position(dust, 3, Vector2(5, 2), Vector2(0, 1))
+    await assert_dust_state(dust, 1, Vector2(0, 5), Vector2(0, 1))
+    await assert_dust_state(dust, 3, Vector2(5, 2), Vector2(0, 1))
     # no new dust was spawned, because it would have been at the same position (5,2)
 
-    await assert_grid_dust(space, 0, 0, [Dust(Vector2(0, 5), 1), Dust(Vector2(5, 2), 3)], [])
+    await assert_grid_state(space, [Dust(Vector2(0, 5), 1), Dust(Vector2(5, 2), 3)], [])
 
 
 @pytest.mark.asyncio
@@ -178,7 +178,7 @@ async def test_add_ship(starknet: Starknet, space_factory):
     ship2 = await deploy_contract(starknet, 'ships/static_ship.cairo')
 
     await space.add_ship(3, 3, ship1.contract_address).invoke(caller_address=ADMIN)
-    await assert_grid_dust(space, 0, 0, [], [Ship(Vector2(3, 3), ship1.contract_address)])
+    await assert_grid_state(space, [], [Ship(Vector2(3, 3), ship1.contract_address)])
 
     await assert_revert(space.add_ship(3, 3, ship2.contract_address).invoke(caller_address=ADMIN))
 
@@ -191,20 +191,20 @@ async def test_next_turn_with_ship(starknet: Starknet, space_factory):
     ship_assertion = Ship(Vector2(0, 3), ship.contract_address)
 
     # Assert grid is empty
-    await assert_grid_dust(space, 0, 0, [], [ship_assertion])
+    await assert_grid_state(space, [], [ship_assertion])
 
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 1), Vector2(0, 1))
-    await assert_grid_dust(space, 0, 0, [Dust(Vector2(0, 1), 1)], [ship_assertion])
+    await assert_dust_state(dust, 1, Vector2(0, 1), Vector2(0, 1))
+    await assert_grid_state(space, [Dust(Vector2(0, 1), 1)], [ship_assertion])
 
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 2), Vector2(0, 1))
-    await assert_dust_position(dust, 2, Vector2(0, 4), Vector2(0, -1))
-    await assert_grid_dust(space, 0, 0, [Dust(Vector2(0, 2), 1), Dust(Vector2(0, 4), 2)], [ship_assertion])
+    await assert_dust_state(dust, 1, Vector2(0, 2), Vector2(0, 1))
+    await assert_dust_state(dust, 2, Vector2(0, 4), Vector2(0, -1))
+    await assert_grid_state(space, [Dust(Vector2(0, 2), 1), Dust(Vector2(0, 4), 2)], [ship_assertion])
 
     # Assert the space owns the dusts
     execution_info = await dust.ownerOf(to_uint(0)).call()
@@ -215,8 +215,8 @@ async def test_next_turn_with_ship(starknet: Starknet, space_factory):
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 1, Vector2(0, 3), Vector2(0, 1))
-    await assert_dust_position(dust, 2, Vector2(0, 3), Vector2(0, -1))
+    await assert_dust_state(dust, 1, Vector2(0, 3), Vector2(0, 1))
+    await assert_dust_state(dust, 2, Vector2(0, 3), Vector2(0, -1))
     # MAX_DUST == 2 so no more dust has been spawned
 
     # Assert the ship earned the dusts at position (0, 3)
@@ -225,14 +225,14 @@ async def test_next_turn_with_ship(starknet: Starknet, space_factory):
     execution_info = await dust.ownerOf(to_uint(1)).call()
     assert execution_info.result == (ship.contract_address,)
 
-    await assert_grid_dust(space, 0, 0, [], [ship_assertion])
+    await assert_grid_state(space, [], [ship_assertion])
 
     # Next turn --------------------------------------------------
     await space.next_turn().invoke(caller_address=ADMIN)
 
-    await assert_dust_position(dust, 3, Vector2(5, 1), Vector2(0, 1))
+    await assert_dust_state(dust, 3, Vector2(5, 1), Vector2(0, 1))
 
-    await assert_grid_dust(space, 0, 0, [Dust(Vector2(5, 1), 3)], [ship_assertion])
+    await assert_grid_state(space, [Dust(Vector2(5, 1), 3)], [ship_assertion])
 
 #
 # Helpers to assert the state of the entire grid
@@ -249,37 +249,33 @@ class Ship(NamedTuple):
     pos: Vector2
     id: int
 
-async def assert_grid_dust(space, x:int, y:int, dusts:List[Dust], ships:List[Ship]):
-    if x >= SPACE_SIZE:
-        assert len(dusts) == 0
-        assert len(ships) == 0
-        return
-    if y >= SPACE_SIZE:
-        assert_grid_dust(space, x+1, 0, dusts, ships)
-    
-    execution_info = await space.get_first_non_empty_cell(x, y).call()
-    (px, py), (lo, hi), ship = execution_info.result.cell
-    if px >= SPACE_SIZE: # => nothing found
-        assert len(dusts) == 0
-        assert len(ships) == 0
-        return
-    assert_dust_id_at(Vector2(px, py), lo, dusts)
-    assert_ship_at(Vector2(px, py), ship, ships)
-    assert_grid_dust(space, x, y+1, dusts, ships)
+async def assert_grid_state(space, dusts:List[Dust], ships:List[Ship]):
+    for x in range(SPACE_SIZE):
+        for y in range(SPACE_SIZE):
+            execution_info = await space.get_first_non_empty_cell(x, y).call()
+            (position_x, position_y), (dust_id_low, _), ship = execution_info.result.cell
+            if position_x >= SPACE_SIZE: # => nothing found
+                assert len(dusts) == 0
+                assert len(ships) == 0
+                return
+            if dust_id_low != 0:
+                assert_dust_id_in_list(Vector2(position_x, position_y), dust_id_low, dusts)
+            if ship != 0:
+                assert_ship_in_list(Vector2(position_x, position_y), ship, ships)
 
-def assert_dust_id_at(position:Vector2, id_lo:int, dusts:List[Dust]):
+def assert_dust_id_in_list(position:Vector2, dust_id_low:int, dusts:List[Dust]):
     for d in dusts:
         if d.pos.x == position.x and d.pos.y == position.y:
-            assert id_lo == d.id, 'Expected dust {id}, at position ({x}, {y}). Got {res}'.format(id=id_lo, x=position.x, y=position.y, res=d.id)
+            assert dust_id_low == d.id, 'Expected dust {id}, at position ({x}, {y}). Got {res}'.format(id=dust_id_low, x=position.x, y=position.y, res=d.id)
             dusts.remove(d)
 
-def assert_ship_at(position:Vector2, ship:int, ships:List[Ship]):
+def assert_ship_in_list(position:Vector2, ship:int, ships:List[Ship]):
     for s in ships:
         if s.pos.x == position.x and s.pos.y == position.y:
             assert ship == s.id, 'Expected ship {id}, at position ({x}, {y}). Got {res}'.format(id=ship, x=position.x, y=position.y, res=s.id)
             ships.remove(s)
 
-async def assert_dust_position(dust, id:int, position:Vector2, direction:Vector2):
+async def assert_dust_state(dust, id:int, position:Vector2, direction:Vector2):
     execution_info = await dust.metadata(to_uint(id-1)).call()
     _, (px, py), (dx, dy) = execution_info.result.metadata
     assert px == position.x
