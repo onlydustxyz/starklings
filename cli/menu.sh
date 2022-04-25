49dc59c57171806f21850738ffd9f429b6dcc500
Olivier Fuxet <olivier.fuxet@gmail.com>
2022-04-25 20:56:02 +0200

    feat: add missing commands to CLI

--- cli/menu.sh
+++ cli/menu.sh
@@ -32,13 +32,36 @@ function mint_boarding_pass() {
     nile invoke starkonquest_boarding_pass mint $to $id_low $id_high
 }
 
+function open_tournament_registration() {
+    nile invoke tournament open_tournament_registration
+}
+
+function close_tournament_registration() {
+    nile invoke tournament close_tournament_registration
+}
+
+function register_ship() {
+    echo "Type the ship address: "
+    read
+    ship_address=${REPLY}
+    nile invoke tournament register $ship_address
+}
+
+function start_tournament() {
+    nile invoke tournament start_tournament
+}
+
 while true; do
-    select yn in "Score" "Dust-Balance" "Boarding-Pass-Balance" "Mint-Boarding-Pass" "Quit"; do
+    select yn in "Score" "Dust-Balance" "Boarding-Pass-Balance" "Mint-Boarding-Pass" "Open-Tournament-Registration" "Close-Tournament-Registration" "Register-Ship" "Start-Tournament" "Quit"; do
         case $yn in
             Score ) score; break;;
             Dust-Balance) dust_balance; break;;
             Boarding-Pass-Balance) boarding_pass_balance; break;;
             Mint-Boarding-Pass) mint_boarding_pass; break;;
+            Open-Tournament-Registration) open_tournament_registration; break;;
+            Close-Tournament-Registration) close_tournament_registration; break;;
+            Register-Ship) register_ship; break;;
+            Start-Tournament) start_tournament; break;;
             Quit ) exit;;
         esac
     done
