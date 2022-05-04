7dd9d00e0877125bf31339bc9565c56ff34c6e07
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-04 11:13:19 +0200

    :sparkles: Print dependencies versions

--- src/utils/starklings_directory.py
+++ src/utils/starklings_directory.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+from packaging import version
+from packaging.version import Version as PackagingVersion
+from typing import Optional
+import tomli
+from logging import getLogger
 
 class StarklingsDirectory:
     def __init__(self, binary_dir_path: Path):
@@ -11,3 +16,71 @@ class StarklingsDirectory:
     @property
     def root_dir_path(self) -> Path:
         return self._binary_dir_path / ".." / ".."
+
+class VersionManager:
+    @staticmethod
+    def parse(version_str: str) -> PackagingVersion:
+        return version.parse(version_str)
+
+    def __init__(self, starklings_directory: StarklingsDirectory) -> None:
+        self._starklings_directory = starklings_directory
+
+    @property
+    def starklings_version(self) -> Optional[PackagingVersion]:
+        path = (
+            self._starklings_directory.root_dir_path
+            / "dist"
+            / "starklings"
+            / "info"
+            / "pyproject.toml"
+        )
+        try:
+            with open(path, "r", encoding="UTF-8") as file:
+                version_s = tomli.loads(file.read())["tool"]["poetry"]["version"]
+                return VersionManager.parse(version_s)
+        except FileNotFoundError:
+            getLogger().warning("Couldn't read Starklings version")
+            return None
+
+    @property
+    def protostar_version(self) -> Optional[PackagingVersion]:
+        path = (
+            self._starklings_directory.root_dir_path
+            / "dist"
+            / "starklings"
+            / "info"
+            / "pyproject.toml"
+        )
+        try:
+            with open(path, "r", encoding="UTF-8") as file:
+                version_s = tomli.loads(file.read())["tool"]["poetry"]["dependencies"][
+                    "protostar"
+                ]["tag"].replace("v", "")
+                return VersionManager.parse(version_s)
+        except FileNotFoundError:
+            getLogger().warning("Couldn't read Protostar version")
+            return None
+
+    @property
+    def cairo_version(self) -> Optional[PackagingVersion]:
+        path = (
+            self._starklings_directory.root_dir_path
+            / "dist"
+            / "starklings"
+            / "info"
+            / "pyproject.toml"
+        )
+        try:
+            with open(path, "r", encoding="UTF-8") as file:
+                version_s = tomli.loads(file.read())["tool"]["poetry"]["dependencies"][
+                    "cairo-lang"
+                ].replace("^", "")
+                return VersionManager.parse(version_s)
+        except FileNotFoundError:
+            getLogger().warning("Couldn't read cairo-lang version")
+            return None
+
+    def print_current_version(self) -> None:
+        print(f"Starklings version: {self.starklings_version or 'unknown'}")
+        print(f"Protostar version: {self.protostar_version or 'unknown'}")
+        print(f"Cairo-lang version: {self.cairo_version or 'unknown'}")
--- src/utils/starklings_directory_test.py
+++ src/utils/starklings_directory_test.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from src.utils.starklings_directory import StarklingsDirectory
+from src.utils.starklings_directory import StarklingsDirectory, VersionManager
 
 
 @pytest.fixture
@@ -10,7 +10,7 @@ def script_root(tmpdir: str) -> Path:
     return Path(tmpdir)
 
 
-def test_protostar_binary_dir_path(script_root: Path):
+def test_starklings_binary_dir_path(script_root: Path):
     starklings_directory = StarklingsDirectory(script_root)
 
     assert starklings_directory.binary_dir_path == script_root
@@ -20,3 +20,11 @@ def test_directory_root_path(script_root: Path):
     starklings_directory = StarklingsDirectory(script_root)
 
     assert starklings_directory.root_dir_path == script_root / ".." / ".."
+
+def test_version_manager():
+    starklings_directory = StarklingsDirectory(Path(__file__).parent)
+    version_manager = VersionManager(starklings_directory)
+
+    assert version_manager.starklings_version > VersionManager.parse("0.0.0")
+    assert version_manager.protostar_version > VersionManager.parse("0.0.0")
+    assert version_manager.cairo_version > VersionManager.parse("0.0.0")
