89d86cbee24755d5e6399af4922ef8010f551250
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-05 09:30:18 +0200

    :sparkles: Subprocess protostar test runner

---
+++ src/exercise_runner/runner.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import subprocess
+
+script_root = Path(__file__).parent / ".." / ".."
+
+
+class ExerciceFailed(Exception):
+    def __init__(self, message):
+        self._message = message
+
+    @property
+    def message(self):
+        return self._message
+
+
+class ProtostarExerciseRunner:
+    async def run(self, exercise_path):
+        test_run = subprocess.run(
+            ["protostar", "test", exercise_path], capture_output=True, cwd=script_root
+        )
+        print(test_run)
+        if len(test_run.stderr) > 0:
+            raise ExerciceFailed(test_run.stderr.decode("utf-8"))
+        if "------- FAILURES --------" in test_run.stdout.decode("utf-8"):
+            raise ExerciceFailed(test_run.stdout.decode("utf-8"))
---
+++ src/exercise_runner/runner_test.py
@@ -0,0 +1,25 @@
+import pytest
+from src.exercise_runner.runner import ExerciceFailed, ProtostarExerciseRunner
+
+
+async def test_protostar_test_runner_success():
+    runner = ProtostarExerciseRunner()
+    await runner.run("tests/test.cairo")
+
+
+async def test_protostar_test_runner_missing_exercise():
+    runner = ProtostarExerciseRunner()
+    with pytest.raises(ExerciceFailed) as error:
+        await runner.run("tests/test_missing.cairo")
+
+
+async def test_protostar_test_runner_failing_exercise():
+    runner = ProtostarExerciseRunner()
+    with pytest.raises(ExerciceFailed) as error:
+        await runner.run("tests/test_failure.cairo")
+
+
+async def test_protostar_test_runner_invalid_exercise():
+    runner = ProtostarExerciseRunner()
+    with pytest.raises(ExerciceFailed) as error:
+        await runner.run("tests/test_invalid.cairo")
