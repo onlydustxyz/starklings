b77ea0620e51d6d20bc9ae4606f471decfae22cd
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-05-05 16:17:41 +0200

    :hammer: make watch and verify classes

--- src/verify.py
+++ src/verify.py
@@ -1,29 +1,29 @@
 from pathlib import Path
 
-exercises = [
-    ("contracts/beginner", ["ex00", "ex01", "ex02", "ex03", "ex04"]),
-    ("contracts/advanced", ["ex00", "ex01"]),
-]
 
+class ExerciseSeeker:
+    def __init__(self, exercises, root_directory: Path):
+        self._exercises = exercises
+        self._directory = root_directory
 
-def is_exercise_done(path: str) -> bool:
-    file = open(path, "r")
-    lines = file.readlines()
+    @staticmethod
+    def _is_exercise_done(path: str) -> bool:
+        file = open(path, "r")
+        lines = file.readlines()
 
-    for line in lines:
-        if line == "# I AM NOT DONE\n":
-            return False
+        for line in lines:
+            if line == "# I AM NOT DONE\n":
+                return False
 
-    return True
+        return True
 
+    def find_next_exercise(self) -> str:
+        for (path, exercise_list) in self._exercises:
+            for exercise in exercise_list:
+                exercise_path = self._directory.joinpath(path).joinpath(
+                    "{}.cairo".format(exercise)
+                )
+                if not self._is_exercise_done(exercise_path):
+                    return exercise_path
 
-def find_next_exercise(script_root: Path) -> str:
-    for (path, exercise_list) in exercises:
-        for exercise in exercise_list:
-            exercise_path = script_root.joinpath(path).joinpath(
-                "{}.cairo".format(exercise)
-            )
-            if not is_exercise_done(exercise_path):
-                return exercise_path
-
-    return None
+        return None
--- src/watch.py
+++ src/watch.py
@@ -8,25 +8,30 @@ from watchdog.events import FileSystemEventHandler
 class Handler(FileSystemEventHandler):
     @staticmethod
     def on_any_event(event):
-        if event.is_directory:
+        if event.event_type == "modified":
+            # TODO: Here we want to run the file tests instead of displaying its path
+            print("Received modified event - {}.".format(event.src_path))
+            return None
+        elif event.is_directory:
             return None
         elif event.event_type == "created":
             return None
-        elif event.event_type == "modified":
-            # TODO: Here we want to run the file tests instead of displaying its path
-            print("Received modified event - {}.".format(event.src_path))
 
 
-def watch(dir_path: Path):
-    event_handler = Handler()
-    observer = Observer()
-    observer.schedule(event_handler, dir_path, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(5)
-    except:
-        observer.stop()
-        print("Error")
+class FilesystemWatcher:
+    def __init__(self, root_dir: Path):
+        self._root_dir = root_dir
 
-    observer.join()
+    def start(self):
+        event_handler = Handler()
+        observer = Observer()
+        observer.schedule(event_handler, self._root_dir, recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            observer.stop()
+            print("Error")
+
+        observer.join()
