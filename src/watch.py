f89d7947dce592acb9cb47e3de60a784f0dafc20
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-05-05 10:41:08 +0200

    :art: run black

--- src/watch.py
+++ src/watch.py
@@ -10,9 +10,9 @@ class Handler(FileSystemEventHandler):
     def on_any_event(event):
         if event.is_directory:
             return None
-        elif event.event_type == 'created':
+        elif event.event_type == "created":
             return None
-        elif event.event_type == 'modified':
+        elif event.event_type == "modified":
             # TODO: Here we want to run the file tests instead of displaying its path
             print("Received modified event - {}.".format(event.src_path))
 
