820134dce348c82ef8ab5748032f42662cc32ff2
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-06 16:32:46 +0200

    :sparkles: Use user repository as root directory instead of the packaged binary one

--- src/exercise_checker/checker_test.py
+++ src/exercise_checker/checker_test.py
@@ -1,25 +1,29 @@
+import os
+from pathlib import Path
 import pytest
 from src.exercise_checker.checker import ExerciceFailed, ProtostarExerciseChecker
 
 
-async def test_protostar_test_runner_success():
-    runner = ProtostarExerciseChecker()
+@pytest.fixture(name="runner")
+def runner_fixture():
+    script_root = Path(os.getcwd())
+    return ProtostarExerciseChecker(script_root)
+
+
+async def test_protostar_test_runner_success(runner):
     await runner.run("tests/test.cairo")
 
 
-async def test_protostar_test_runner_missing_exercise():
-    runner = ProtostarExerciseChecker()
+async def test_protostar_test_runner_missing_exercise(runner):
     with pytest.raises(ExerciceFailed):
         await runner.run("tests/test_missing.cairo")
 
 
-async def test_protostar_test_runner_failing_exercise():
-    runner = ProtostarExerciseChecker()
+async def test_protostar_test_runner_failing_exercise(runner):
     with pytest.raises(ExerciceFailed):
         await runner.run("tests/test_failure.cairo")
 
 
-async def test_protostar_test_runner_invalid_exercise():
-    runner = ProtostarExerciseChecker()
+async def test_protostar_test_runner_invalid_exercise(runner):
     with pytest.raises(ExerciceFailed):
         await runner.run("tests/test_invalid.cairo")
