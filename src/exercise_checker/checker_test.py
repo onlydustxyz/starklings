9082c28c378c0a1d3d77deb6e7cc36f719d2d2f1
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-06 12:10:25 +0200

    :truck: Rename exercise checker

--- src/exercise_checker/checker_test.py
+++ src/exercise_checker/checker_test.py
@@ -1,25 +1,25 @@
 import pytest
-from src.exercise_runner.runner import ExerciceFailed, ProtostarExerciseRunner
+from src.exercise_checker.checker import ExerciceFailed, ProtostarExerciseChecker
 
 
 async def test_protostar_test_runner_success():
-    runner = ProtostarExerciseRunner()
+    runner = ProtostarExerciseChecker()
     await runner.run("tests/test.cairo")
 
 
 async def test_protostar_test_runner_missing_exercise():
-    runner = ProtostarExerciseRunner()
+    runner = ProtostarExerciseChecker()
     with pytest.raises(ExerciceFailed):
         await runner.run("tests/test_missing.cairo")
 
 
 async def test_protostar_test_runner_failing_exercise():
-    runner = ProtostarExerciseRunner()
+    runner = ProtostarExerciseChecker()
     with pytest.raises(ExerciceFailed):
         await runner.run("tests/test_failure.cairo")
 
 
 async def test_protostar_test_runner_invalid_exercise():
-    runner = ProtostarExerciseRunner()
+    runner = ProtostarExerciseChecker()
     with pytest.raises(ExerciceFailed):
         await runner.run("tests/test_invalid.cairo")
