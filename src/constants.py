fe294767558daf781c9a5be8c2db940555e00fbf
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-05-06 14:49:04 +0200

    :building_construction: rename contracts to exercises

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,4 @@
 exercise_files_architecture = [
-    ("contracts/beginner", ["ex00", "ex01", "ex02", "ex03", "ex04"]),
-    ("contracts/advanced", ["ex00", "ex01"]),
+    ("exercises/beginner", ["ex00", "ex01", "ex02", "ex03", "ex04"]),
+    ("exercises/advanced", ["ex00", "ex01"]),
 ]
