9e32802f1d52dd5fd5f4e2153a24b9aa153a4e2f
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-11 11:41:31 +0200

    feat(Syntax): struct exercise

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
 exercise_files_architecture = [
-    ("exercises/syntax", ["syntax01", "syntax02", "syntax03", "syntax04"]),
+    ("exercises/syntax", ["syntax01", "syntax02", "syntax03", "syntax04", "syntax05"]),
     ("exercises/beginner", ["ex00", "ex01", "ex02", "ex03", "ex04"]),
     ("exercises/advanced", ["ex00", "ex01"]),
 ]
