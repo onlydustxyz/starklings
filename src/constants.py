7ac8ab5494bdfacb14a83225bfef6499ee544f3b
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-15 19:44:51 +0200

    feat(Recursion): pointer update

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 exercise_files_architecture = [
     ("exercises/syntax", ["syntax01", "syntax02", "syntax03", "syntax04", "syntax05"]),
     ("exercises/beginner", ["ex00", "ex01", "ex02", "ex03", "ex04"]),
-    ("exercises/recursions", ["recursion01", "array01"]),
+    ("exercises/recursions", ["recursion01", "array01", "array02"]),
     ("exercises/advanced", ["ex00", "ex01"]),
 ]
