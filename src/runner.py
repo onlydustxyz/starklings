17a7c2d3ed738569c7b511294fab26f46208078a
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-19 12:46:39 -0400

    feat: explicit the fact that the CLI must run in the repository

--- src/runner.py
+++ src/runner.py
@@ -1,6 +1,7 @@
 import asyncio
 import contextlib
 from pathlib import Path
+import sys
 from threading import Lock
 from time import sleep
 from src.exercise_checker.checker import ExerciceFailed, check_exercise
@@ -16,7 +17,11 @@ class Runner:
     def __init__(self, root_path: Path):
         self._file_watcher = FileWatcher(root_path)
         self._exercise_seeker = ExerciseSeeker(exercise_files_architecture, root_path)
-        prompt.on_watch_start(self._exercise_seeker.find_next_exercise())
+        try:
+            prompt.on_watch_start(self._exercise_seeker.find_next_exercise())
+        except FileNotFoundError:
+            prompt.on_file_not_found()
+            sys.exit(1)
 
     def on_file_changed(self, _):
         asyncio.run(self._check_exercise())
@@ -34,7 +39,10 @@ class Runner:
                 prompt.on_exercise_failure(next_exercise_path, error.message)
 
     def run(self):
-        with contextlib.suppress(KeyboardInterrupt):
-            self._file_watcher.start(self.on_file_changed)
-            while True:
-                sleep(5)
+        try:
+            with contextlib.suppress(KeyboardInterrupt):
+                self._file_watcher.start(self.on_file_changed)
+                while True:
+                    sleep(5)
+        except FileNotFoundError:
+            prompt.on_file_not_found()
