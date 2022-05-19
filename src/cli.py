e044f43113d94e3965c015de6123b48cddf3dfde
Stanislas Bernard <bernardstanislas@gmail.com>
2022-05-19 14:09:41 -0400

    feat: add monitoring using Sentry

--- src/cli.py
+++ src/cli.py
@@ -1,9 +1,19 @@
 from pathlib import Path
+import sentry_sdk
 from .runner import Runner
 from .verify import ExerciseSeeker
 from .constants import exercise_files_architecture
 from .utils.starklings_directory import StarklingsDirectory, VersionManager
 from .solution import SolutionPatcher
+
+
+sentry_sdk.init(
+    "https://73212d09152344fd8e351ef180b8fa75@o1254095.ingest.sentry.io/6421829",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+)
 
 
 async def cli(args, script_root: Path):
@@ -14,10 +24,12 @@ async def cli(args, script_root: Path):
         version_manager.print_current_version()
 
     if args.watch:
+        sentry_sdk.capture_message("Starting the watch mode")
         runner = Runner(script_root)
         runner.run()
 
     if args.verify:
+        sentry_sdk.capture_message("Verifying all the exercises")
         seeker = ExerciseSeeker(exercise_files_architecture, script_root)
         exercise_path = seeker.find_next_exercise()
 
@@ -27,6 +39,9 @@ async def cli(args, script_root: Path):
             print(exercise_path)
 
     if args.solution:
+        sentry_sdk.capture_message(
+            "Displaying the solution", path_to_solution=args.solution
+        )
         displayer = SolutionPatcher(args.solution)
         solution = displayer.get_solution()
         if solution:
