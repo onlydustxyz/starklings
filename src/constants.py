c58bd53e320b050514ea270d8ba014d926fc60b2
tekkac <98529704+tekkac@users.noreply.github.com>
2022-05-24 15:59:51 +0200

    feat(Exercises): add an exercise on strings

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
 exercise_files_architecture = [
     ("exercises/syntax", ["syntax01", "syntax02", "syntax03", "syntax04", "syntax05"]),
-    ("exercises/strings", ["strings00"]),
     ("exercises/beginner", ["ex00", "ex01", "ex02", "ex03", "ex04"]),
+    ("exercises/strings", ["strings00", "strings01"]),
     (
         "exercises/implicit_arguments",
         ["implicit_arguments01", "implicit_arguments02", "implicit_arguments03"],
