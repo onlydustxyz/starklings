975a24d73ac5a3eed45909b9fea1b7b9c537c032
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2022-05-30 15:47:08 -0400

    chore: remove beginners exercises

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,5 @@
 exercise_files_architecture = [
     ("exercises/syntax", ["syntax01", "syntax02", "syntax03", "syntax04", "syntax05"]),
-    ("exercises/beginner", ["ex00", "ex01", "ex02", "ex03", "ex04"]),
     ("exercises/strings", ["strings00", "strings01"]),
     (
         "exercises/implicit_arguments",
