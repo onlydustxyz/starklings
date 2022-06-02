9a91ec89766e1da65b49f0d3a771b410ee86c6b9
Stanislas Bernard <bernardstanislas@gmail.com>
2022-06-02 11:56:15 +0200

    fix: version printing relies on the packaged pyproject.toml

--- src/utils/starklings_directory_test.py
+++ src/utils/starklings_directory_test.py
@@ -1,29 +1,8 @@
-from pathlib import Path
-
-import pytest
-
 from src.utils.starklings_directory import StarklingsDirectory, VersionManager
 
 
-@pytest.fixture(name="script_root")
-def script_root_fixture(tmpdir: str) -> Path:
-    return Path(tmpdir)
-
-
-def test_starklings_binary_dir_path(script_root: Path):
-    starklings_directory = StarklingsDirectory(script_root)
-
-    assert starklings_directory.binary_dir_path == script_root
-
-
-def test_directory_root_path(script_root: Path):
-    starklings_directory = StarklingsDirectory(script_root)
-
-    assert starklings_directory.root_dir_path == script_root / ".." / ".."
-
-
 def test_version_manager():
-    starklings_directory = StarklingsDirectory(Path(__file__).parent)
+    starklings_directory = StarklingsDirectory()
     version_manager = VersionManager(starklings_directory)
 
     assert version_manager.starklings_version > VersionManager.parse("0.0.0")
