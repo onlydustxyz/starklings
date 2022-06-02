db6ca6dca1ab7b42cea20f19e5ffe88f7799cd31
Codiumdium <74763842+Codiumdium@users.noreply.github.com>
2022-06-02 11:41:38 +0400

    fix #124 - a bash script for adding new exercises

---
+++ scripts/add_new_exercise.sh
@@ -0,0 +1,32 @@
+#! /bin/env bash
+# Add a new exercise
+
+# set -eux
+
+ROOT_DIR=$PWD
+EXERCISES="$PWD/exercises"
+EXERCISE_PATCHES="$PWD/.patches"
+CONSTANTS="$PWD/src/constants.py"
+
+if [[ $(basename "$ROOT_DIR") != "starklings" ]]; then echo "Execute $0 at the root of the starklings project!"; exit; fi
+if ([ -z "$1" ] || [ ! -f "$1" ]); then echo "Please provide a valide path to the exercice!"; exit; fi
+if ([ -z "$2" ] || [ ! -f "$2" ]); then echo "Please provide a valide path to the exercice solution!"; exit; fi
+if [ -z "$3" ]; then echo "Please provide a valide name for the exercice folder!"; exit; fi
+
+EXERCISE=$1
+EXERCISE_SOLUTION=$2
+EXERCISE_FOLDER=$3
+
+mkdir -p "$EXERCISES/$EXERCISE_FOLDER"
+mkdir -p "$EXERCISE_PATCHES/$EXERCISE_FOLDER"
+
+diff -Nb -U0 $EXERCISE $EXERCISE_SOLUTION > "$EXERCISE_PATCHES/$EXERCISE_FOLDER/$(basename $EXERCISE).patch"
+cp $EXERCISE "$EXERCISES/$EXERCISE_FOLDER/$(basename $EXERCISE)"
+
+echo "Done"
+echo "Add or adapt '("exercises/$EXERCISE_FOLDER", ["$(basename $EXERCISE '.cairo')"]),' in $CONSTANTS"
+
+
+
+
+
