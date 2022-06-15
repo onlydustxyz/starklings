5003140d99e29bb532e863182e161019e7431ce3
tekkac <98529704+tekkac@users.noreply.github.com>
2022-06-15 15:27:18 +0200

    fix: update new exercise script

--- scripts/add_new_exercise.sh
+++ scripts/add_new_exercise.sh
@@ -6,7 +6,7 @@
 ROOT_DIR=$PWD
 EXERCISES="$PWD/exercises"
 EXERCISE_PATCHES="$PWD/.patches"
-CONSTANTS="$PWD/src/constants.py"
+COURSE="$PWD/src/exercises/__init__.py"
 
 if [[ $(basename "$ROOT_DIR") != "starklings" ]]; then echo "Execute $0 at the root of the starklings project!"; exit; fi
 if ([ -z "$1" ] || [ ! -f "$1" ]); then echo "Please provide a valid path to the exercise!"; exit; fi
@@ -24,4 +24,4 @@ diff -Nb -U0 $EXERCISE $EXERCISE_SOLUTION > "$EXERCISE_PATCHES/$EXERCISE_FOLDER/
 cp $EXERCISE "$EXERCISES/$EXERCISE_FOLDER/$(basename $EXERCISE)"
 
 echo "Done"
-echo "Add or adapt '("exercises/$EXERCISE_FOLDER", ["$(basename $EXERCISE '.cairo')"]),' in $CONSTANTS"
+echo "Update course in $COURSE"
