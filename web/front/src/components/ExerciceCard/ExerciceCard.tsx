cf0812abdebc3dc39475c4cadb84aab08c7d4f43
S.K <cecilia.mcszk@gmail.com>
2022-08-03 03:50:04 +0200

    add achievements styles + cpomponent

--- web/front/src/components/ExerciceCard/ExerciceCard.tsx
+++ web/front/src/components/ExerciceCard/ExerciceCard.tsx
@@ -1,15 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import ExerciceContent from '../ExerciceContent/ExerciceContent';
 
 
 interface ExerciceCardProps {
+  name?: string,
   exerciceTitle?: string,
   status?: string; // success, wip || not-started
 }
 
-const ExerciceCard: FC<ExerciceCardProps> = ({exerciceTitle, status}) => (
-    <div className='exercice-card'>
+const ExerciceCard: FC<ExerciceCardProps> = ({exerciceTitle, status}) => {
+  const [clickedCard, setClickedCard] = useState('');
+  const cardHandler = (event: React.MouseEvent<HTMLElement>) => { 
+    event.preventDefault();
+  // set const card event current target
+  const card = (event: React.MouseEvent<HTMLElement>) => { return event.currentTarget; };
+  setClickedCard(card.name);
+  };
+
+  return(
+    <div className='exercice-card' onClick={cardHandler}>
       <h3 className={`card-header ${status}`}>{exerciceTitle}</h3>
+      {clickedCard !== '' ? <ExerciceContent listOfExercices={['Syntax00', 'Syntax01', 'Syntax02']} lastSuccessfulExercice={1}/> : null}
     </div>
-);
+  );
+}
 
 export default ExerciceCard;
