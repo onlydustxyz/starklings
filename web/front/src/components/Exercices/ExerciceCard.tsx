5c19bcd74f77b1058373ecdf5061e77c028fabef
Seona <iamseonab@tuta.io>
2022-09-14 00:22:38 +0200

    delete useless variable

--- web/front/src/components/Exercices/ExerciceCard.tsx
+++ web/front/src/components/Exercices/ExerciceCard.tsx
@@ -27,8 +27,7 @@ const ExerciceCard: FC<ExerciceCardProps> = ({exerciceTitle, status}) => {
           // replace underscores with space
           data[0] = data[0].replace(/_/g, ' ')
           // check if strg contains number
-          let hasNum : boolean = data[0].match(/\d/) ? true : false
-          if(hasNum) {
+          if(data[0].match(/\d/)) {
             let formattedData = data[0].slice(-2)
             formattedData = data[0].slice(0, -2) + ' ' + formattedData
             tmpList.push(formattedData)
