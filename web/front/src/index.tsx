8480667f3381e95667b5a227d30277b8917e93af
Seona <iamseonab@tuta.io>
2022-09-14 11:37:08 +0200

    delete comment

--- web/front/src/index.tsx
+++ web/front/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import 'index.css';
 import 'App.sass';
 import App from 'App';
 import { StarknetProvider, getInstalledInjectedConnectors } from '@starknet-react/core'
