736b15802949f0a01408c653cb031c8bcfc38da8
Seona <iamseonab@tuta.io>
2022-09-14 11:35:11 +0200

    delete useless file

--- web/front/src/index.tsx
+++ web/front/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 //import 'index.css';
 import 'App.sass';
 import App from 'App';
-import reportWebVitals from 'reportWebVitals';
 import { StarknetProvider, getInstalledInjectedConnectors } from '@starknet-react/core'
 
 const root = ReactDOM.createRoot(
@@ -19,8 +18,3 @@ root.render(
     </StarknetProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
