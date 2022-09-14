0f9ca93c0645fd6f7cba993c2f86f756415ad266
Seona <iamseonab@tuta.io>
2022-09-14 11:33:52 +0200

    delete css file and add sass file for wallet

--- web/front/src/index.tsx
+++ web/front/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'index.css';
+//import 'index.css';
+import 'App.sass';
 import App from 'App';
 import reportWebVitals from 'reportWebVitals';
 import { StarknetProvider, getInstalledInjectedConnectors } from '@starknet-react/core'
