ab9d079cce2fc51e14fc1a2027317c32451cd4ad
internos <amajidsinar@gmail.com>
2022-09-16 21:02:26 +0700

    feat: remove unused code

--- starklings-backend/starklings_backend/models.py
+++ starklings-backend/starklings_backend/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
-# from db import Base
 
 Base = declarative_base()
 
